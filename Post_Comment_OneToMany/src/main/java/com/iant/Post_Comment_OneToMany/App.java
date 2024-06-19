b14fa834d0d2afe7c0c41680d5391cddff9cd3eb
Shaharukh-Shaikh <sharukhshaikh6596@gmail.com>
2024-06-19 21:36:56 +0530

    Post Have Many Comments

---
+++ Post_Comment_OneToMany/src/main/java/com/iant/Post_Comment_OneToMany/App.java
@@ -0,0 +1,13 @@
+package com.iant.Post_Comment_OneToMany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
